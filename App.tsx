ea2a11dd216df50bc6d37318d8d3d2437756ddc0
Matheus Guimarães <matheusguim13@gmail.com>
2023-01-25 20:27:12 -0300

    modulo 2

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme/index'
+import Groups from '@screens/Groups';
+import {  StatusBar } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
+import { Principal } from '@screens/Principal/style';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
+  
+    return (
+    
+    <Principal>
+      <ThemeProvider theme={theme}>
+        
+        { fontsLoaded  ? <Groups/> : <Loading/>}
+  
+        <StatusBar
+          barStyle={'default'}
+          backgroundColor={'transparent'}
+          translucent
+        
+        />
+      </ThemeProvider>
+    </Principal>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,15 @@
+import  { Container} from "./styles";
+import { Header } from "@components/Header";
+
+export default function Groups(){
+  return(
+    <>
+      <Container>
+      <Header showBackButton/>
+
+
+      </Container>
+    
+    </>
+  )
+}
