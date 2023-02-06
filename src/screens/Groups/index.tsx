163f45a5626c60537a7920386bddad2cb78fb424
Matheus Guimarães <matheusguim13@gmail.com>
2023-02-06 16:04:07 -0300

    GrooupCards com Buttons

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,19 +2,47 @@ import  { Container} from "./styles";
 import { Header } from "@components/Header";
 import { Highlights } from "@components/Highlights";
 import { GroupCard } from "@components/Groupcard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
+import {Button} from "@components/Button";
 
 export default function Groups(){
+  const [groups, setGroups] = useState([])
+
+
   return(
     <>
       <Container>
-      <Header showBackButton/>
+      <Header   />
 
       <Highlights
       title="Turmas"
       subtitle="Jogue com sua turma"
       />
+      
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem= {({item}) => (
+        
+        <GroupCard
+         title={item}
+         
+         />)}
 
-      <GroupCard title="Galera do Ignite"/>
+          contentContainerStyle={groups.length === 0 && {flex:1} }
+
+         ListEmptyComponent = {() => (
+          <ListEmpty
+          message="Que tal adicionar uma lista?"
+          />
+
+         )}
+        
+      />
+
+      <Button title="Criar Turma"/>
       </Container>
     
     </>
